1633f80d1cf7dc5f9abd86741ee36c6f1d078302
Charlie Huguenard <charliehuge@users.noreply.github.com>
2026-04-04 11:54:10 -0400

    delete UE flower bed show controller (#6)

--- FlowerBeds/Source/FlowerBeds/FlowerBeds.cpp
+++
@@ -1,6 +0,0 @@
-#include "FlowerBeds.h"
-#include "Modules/ModuleManager.h"
-
-DEFINE_LOG_CATEGORY(LogFlowerBeds);
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlowerBeds, "FlowerBeds" );
